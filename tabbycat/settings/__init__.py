03ccc02253b1ba43db534e6adbf0d6f1104cc548
Miftahul Faris <miftahulfaris.net@gmail.com>
2026-05-08 16:38:54 +0600

    feat: implement environment-based settings management and configure Render environment overrides

--- tabbycat/settings/__init__.py
+++ tabbycat/settings/__init__.py
@@ -24,15 +24,15 @@ base_settings = [
 if os.environ.get('GITHUB_CI', '') and bool(os.environ['GITHUB_CI']):
     base_settings.append('github.py')
     root.info('SPLIT_SETTINGS: imported github.py')
-elif os.environ.get('IN_DOCKER', '') and bool(int(os.environ['IN_DOCKER'])):
-    base_settings.append('docker.py')
-    root.info('SPLIT_SETTINGS: imported docker.py')
-elif os.environ.get('ON_HEROKU', ''):
-    base_settings.append('heroku.py')
-    root.info('SPLIT_SETTINGS: imported heroku.py')
 elif os.environ.get('ON_RENDER', ''):
     base_settings.append('render.py')
     root.info('SPLIT_SETTINGS: imported render.py')
+elif os.environ.get('ON_HEROKU', ''):
+    base_settings.append('heroku.py')
+    root.info('SPLIT_SETTINGS: imported heroku.py')
+elif os.environ.get('IN_DOCKER', '') and bool(int(os.environ['IN_DOCKER'])):
+    base_settings.append('docker.py')
+    root.info('SPLIT_SETTINGS: imported docker.py')
 else:
     base_settings.append('local.py')
     if os.environ.get('LOCAL_DEVELOPMENT', ''):
